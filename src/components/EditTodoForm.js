10d1c1d7298161a2f3e3ce7a453fce854c29e95e
CaroAK <CarolineAliciaKlein@icloud.com>
2024-08-03 23:30:51 -0400

    Update and delete tasks (finished)

--- src/components/EditTodoForm.js
+++ src/components/EditTodoForm.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
+// Use state keeps track of what users type in
 
-export const EditTodoForm = () => {
+export const EditTodoForm = ({editTodo, task}) => {
+    const [value, setvalue] = useState(task.task)
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        editTodo(value, task.id)
+        setvalue("")
+    }
   return (
-    <div>EditTodoForm</div>
+    <form className='TodoForm' onSubmit={handleSubmit}>
+        <input type="text" className='todo-input' 
+        value={value} placeholder='Update Task' 
+        onChange={(e) => setvalue(e.target.value)}/>
+        <button type='submit' className='todo-btn'>Update Task</button>
+    </form>
   )
 }
